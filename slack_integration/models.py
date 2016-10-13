a1905c926456da453b12d5a2918b7e2f4d9acf3f
Quantum <quantum2048@gmail.com>
2016-10-13 19:20:07 -0400

    Permit longer token names

--- slack_integration/models.py
+++ slack_integration/models.py
@@ -8,9 +8,9 @@ from solo.models import SingletonModel
 class SlackIntegration(SingletonModel):
     team_id = models.CharField(max_length=20, blank=True, verbose_name=_('slack team id'))
     team_name = models.CharField(max_length=100, blank=True, verbose_name=_('slack team name'))
-    access_token = models.CharField(max_length=50, blank=True, verbose_name=_('slack access token'))
+    access_token = models.CharField(max_length=100, blank=True, verbose_name=_('slack access token'))
     bot_name = models.CharField(max_length=30, blank=True, verbose_name=_('slack bot id'))
-    bot_token = models.CharField(max_length=50, blank=True, verbose_name=_('slack bot token'))
+    bot_token = models.CharField(max_length=100, blank=True, verbose_name=_('slack bot token'))
     error_channel = models.CharField(max_length=20, blank=True, verbose_name=_('slack error channel'),
                                      help_text=_('The channel where Django error posts go, and also acts '
                                                  'as the default channel.'))
