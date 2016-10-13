2cd2c383f1d731ad3de01145fcbd5713f3166f60
Quantum <quantum2048@gmail.com>
2016-10-13 19:09:01 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+from slack_integration import VERSION
+
+setup(
+    name='django-slack-integration',
+    version='.'.join(map(str, VERSION)),
+    description='Slack integration for Django.',
+    author='Quantum',
+    author_email='quantum@dmoj.ca',
+    url='https://github.com/DMOJ/django-slack-integration',
+    keywords='django slack messaging',
+    packages=find_packages(),
+    include_package_data=True,
+    license='GNU AGPLv3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Communications :: Chat',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: User Interfaces',
+    ],
+    zip_safe=False,
+)
