ec94ec4d69300803a0b59f6cd4862e4f88fe3a35
Roger Fu <me@kirito.ca>
2022-12-26 13:03:56 -0500

    Use `path` instead of deprecated `url`

--- slack_integration/urls.py
+++ slack_integration/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 
 from slack_integration.views.callback import message_callback
 from slack_integration.views.channels import default_channel_select
@@ -6,8 +6,8 @@ from slack_integration.views.oauth import start_oauth, oauth_callback
 
 app_name = 'slack'
 urlpatterns = [
-    url('^oauth/$', start_oauth, name='global_oauth'),
-    url('^oauth/callback/$', oauth_callback, name='global_oauth_callback'),
-    url('^channel/default/$', default_channel_select, name='default_channel_select'),
-    url('^callback/message/$', message_callback, name='message_callback'),
+    path('oauth/', start_oauth, name='global_oauth'),
+    path('oauth/callback/', oauth_callback, name='global_oauth_callback'),
+    path('channel/default/', default_channel_select, name='default_channel_select'),
+    path('callback/message/', message_callback, name='message_callback'),
 ]
